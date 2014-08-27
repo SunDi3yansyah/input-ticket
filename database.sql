1ff2772a59739b28ebcc33b9f386cff8442d7a21
SunDi3yansyah <adieyogyakarta@gmail.com>
2014-08-27 07:18:23 +0700

    update

--- database.sql
+++ database.sql
@@ -1,31 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 2.11.9.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (i686)
 --
--- Host: localhost
--- Waktu pembuatan: 11. Desember 2013 jam 08:32
--- Versi Server: 5.0.67
--- Versi PHP: 5.2.6
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+-- Host: localhost    Database: tiket
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `tugas`
+-- Table structure for table `pesawat`
 --
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pesawat`
---
-
-CREATE TABLE IF NOT EXISTS `pesawat` (
+DROP TABLE IF EXISTS `pesawat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pesawat` (
   `no_reservasi` varchar(10) NOT NULL,
   `nama_pemesan` varchar(35) NOT NULL,
   `tujuan` varchar(15) NOT NULL,
@@ -35,10 +32,25 @@ CREATE TABLE IF NOT EXISTS `pesawat` (
   `total` int(11) NOT NULL,
   `jenis_tiket` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data untuk tabel `pesawat`
+-- Dumping data for table `pesawat`
 --
 
-INSERT INTO `pesawat` (`no_reservasi`, `nama_pemesan`, `tujuan`, `biaya_tujuan`, `biaya_pokok`, `potongan`, `total`, `jenis_tiket`) VALUES
-('12345', 'SunDi3yansyah', 'Yogyakarta', 350000, 100000, 262500, 187500, 'Promosi');
+LOCK TABLES `pesawat` WRITE;
+/*!40000 ALTER TABLE `pesawat` DISABLE KEYS */;
+INSERT INTO `pesawat` VALUES ('12345','SunDi3yansyah','Yogyakarta',350000,100000,262500,187500,'Promosi');
+/*!40000 ALTER TABLE `pesawat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-27  7:16:59
